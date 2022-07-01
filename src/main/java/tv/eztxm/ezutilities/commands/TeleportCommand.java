23727c82a5639214da48b621c9461aad3d09ea79
ezTxmMC <96041552+ezTxmMC@users.noreply.github.com>
2022-07-01 03:06:31 +0200

    Added TeleportCommand.java

---
+++ src/main/java/tv/eztxm/ezutilities/commands/TeleportCommand.java
@@ -0,0 +1,14 @@
+package tv.eztxm.ezutilities.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class TeleportCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        return true;
+    }
+}
