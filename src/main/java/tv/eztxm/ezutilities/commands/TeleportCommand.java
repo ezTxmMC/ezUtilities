0aab9975ff48eae7ed95e1e86575f7ce153e6acc
ezTxmMC <96041552+ezTxmMC@users.noreply.github.com>
2022-07-01 03:19:32 +0200

    Added context and function to case 2

--- src/main/java/tv/eztxm/ezutilities/commands/TeleportCommand.java
+++ src/main/java/tv/eztxm/ezutilities/commands/TeleportCommand.java
@@ -20,10 +20,25 @@ public class TeleportCommand implements CommandExecutor {
                     if (target == null) player.sendMessage(FileManager.getMessage("PlayerNotOnline"));
                     else {
                         player.teleport(target);
+                        player.sendMessage(FileManager.getMessage("TeleportTo").replace("%target%", args[0]));
                     }
                 }
                 case 2 -> {
-
+                    Player target1 = Bukkit.getPlayer(args[0]);
+                    Player target2 = Bukkit.getPlayer(args[1]);
+                    if (target1 == null) player.sendMessage(FileManager.getMessage("PlayerNotOnline"));
+                    else {
+                        if (target2 == null) player.sendMessage(FileManager.getMessage("PlayerNotOnline"));
+                        else {
+                            target1.teleport(target2);
+                            if (player == target1) {
+                                player.sendMessage(FileManager.getMessage("TeleportTo").replace("%target%", args[1]));
+                                return true;
+                            }
+                            target1.sendMessage(FileManager.getMessage("TeleportTo2").replace("%target1%", args[0]).replace("%target2%", args[1]));
+                            player.sendMessage();
+                        }
+                    }
                 }
                 case 3 -> {
 
