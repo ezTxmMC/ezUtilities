eec29f247daf196c6296422cebfaf89c3e290f2d
ezTxmMC <96041552+ezTxmMC@users.noreply.github.com>
2022-07-01 03:09:39 +0200

    Added arguments

--- src/main/java/tv/eztxm/ezutilities/commands/TeleportCommand.java
+++ src/main/java/tv/eztxm/ezutilities/commands/TeleportCommand.java
@@ -3,12 +3,38 @@ package tv.eztxm.ezutilities.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import tv.eztxm.ezutilities.utils.FileManager;
 
 public class TeleportCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) return true;
+        if (!player.hasPermission("ezutilities.teleport")) player.sendMessage(FileManager.getMessage("NoPerms"));
+        else {
+            switch (args.length) {
+                case 1 -> {
 
+                }
+                case 2 -> {
+
+                }
+                case 3 -> {
+
+                }
+                case 4 -> {
+
+                }
+                case 5 -> {
+
+                }
+                case 6 -> {
+                    
+                }
+            }
+            player.sendMessage(FileManager.getMessage("HowToUseTeleport"));
+        }
         return true;
     }
 }
