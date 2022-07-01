61269d539284d617b135d99930e1cac1de20ce6f
ezTxmMC <96041552+ezTxmMC@users.noreply.github.com>
2022-07-01 03:11:11 +0200

    Added case 1 with to player teleport

--- src/main/java/tv/eztxm/ezutilities/commands/TeleportCommand.java
+++ src/main/java/tv/eztxm/ezutilities/commands/TeleportCommand.java
@@ -1,5 +1,6 @@
 package tv.eztxm.ezutilities.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,7 +16,11 @@ public class TeleportCommand implements CommandExecutor {
         else {
             switch (args.length) {
                 case 1 -> {
-
+                    Player target = Bukkit.getPlayer(args[0]);
+                    if (target == null) player.sendMessage(FileManager.getMessage("PlayerNotOnline"));
+                    else {
+                        player.teleport(target);
+                    }
                 }
                 case 2 -> {
 
@@ -30,7 +35,7 @@ public class TeleportCommand implements CommandExecutor {
 
                 }
                 case 6 -> {
-                    
+
                 }
             }
             player.sendMessage(FileManager.getMessage("HowToUseTeleport"));
