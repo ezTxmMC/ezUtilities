6f3dd1fe49042f19facc4fa1480482df6dc12e07
ezTxmMC <96041552+ezTxmMC@users.noreply.github.com>
2022-07-01 02:30:21 +0200

    Added onQuit() to QuitListener

--- src/main/java/tv/eztxm/ezutilities/listeners/QuitListener.java
+++ src/main/java/tv/eztxm/ezutilities/listeners/QuitListener.java
@@ -1,6 +1,19 @@
 package tv.eztxm.ezutilities.listeners;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import tv.eztxm.ezutilities.utils.FileManager;
 
 public class QuitListener implements Listener {
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        if (FileManager.getBoolean("CustomQuitMessage")) {
+            event.setQuitMessage(FileManager.getMessage("CustomQuitMessage")
+                    .replace("%player%", player.getName()));
+        }
+    }
 }
